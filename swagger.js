4bec07455e1e10966d9ffb2d9ec7d897441c9599
eduardoRduraes <eduardoduraes.bsi@gmail.com>
2022-05-30 17:42:41 -0300

    commit com correcao do arquivo swagger.json

--- swagger.js
+++ swagger.js
@@ -4,23 +4,9 @@ const swaggerAutogen = require("swagger-autogen")();
 
 const outputFile = "./src/swagger.json";
 
-const createUser =
-    "src/modules/users/useCases/createUser/CreateUserController.ts";
-const listAllUsers =
-    "src/modules/users/useCases/listAllUsers/ListAllUsersController.ts";
-const showUserProfile =
-    "src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts";
-const turnUserAdmin =
-    "src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts";
 const index = "src/index.ts";
 
-const usersFiles = [
-    index,
-    createUser,
-    listAllUsers,
-    showUserProfile,
-    turnUserAdmin,
-];
+const usersFiles = [index];
 
 console.log(usersFiles);
 
